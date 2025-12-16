24eb833981a201e30597bc489838e0722d789901
Lucas Carlson <lucas.carlson@neighborlysoftware.com>
2025-12-15 21:35:39 -0500

    test: add todo API integration test and fix solution structure

---
+++ backend/todo-api.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using todo_api.Data;
+
+namespace todo_api.IntegrationTests;
+
+public class CustomWebApplicationFactory
+    : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            var descriptor = services.Single(
+                d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+
+            services.Remove(descriptor);
+            var connection = new SqliteConnection("Data Source=:memory:");
+            connection.Open();
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlite(connection);
+            });
+
+            var sp = services.BuildServiceProvider();
+            using var scope = sp.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            db.Database.Migrate();
+        });
+    }
+
+}
---
+++ backend/todo-api.IntegrationTests/Tests/TodosApiTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using FluentAssertions;
+using todo_api.Dtos;
+using Xunit;
+
+namespace todo_api.IntegrationTests;
+
+public class TodosApiTests
+    : IClassFixture<CustomWebApplicationFactory>
+{
+    private readonly HttpClient _client;
+
+    public TodosApiTests(CustomWebApplicationFactory factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Create_and_get_todo_returns_expected_result()
+    {
+        var createRequest = new CreateTodoRequest
+        {
+            Title = "Integration test todo"
+        };
+
+        var createResponse = await _client.PostAsJsonAsync(
+            "/api/todos", createRequest);
+
+        createResponse.StatusCode.Should().Be(HttpStatusCode.Created);
+
+        var createdTodo =
+            await createResponse.Content.ReadFromJsonAsync<TodoResponse>();
+
+        createdTodo.Should().NotBeNull();
+        createdTodo!.Title.Should().Be("Integration test todo");
+        createdTodo.IsCompleted.Should().BeFalse();
+
+        var getResponse = await _client.GetAsync(
+            $"/api/todos/{createdTodo.Id}");
+
+        getResponse.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        var fetchedTodo =
+            await getResponse.Content.ReadFromJsonAsync<TodoResponse>();
+
+        fetchedTodo.Should().NotBeNull();
+        fetchedTodo!.Id.Should().Be(createdTodo.Id);
+        fetchedTodo.Title.Should().Be(createdTodo.Title);
+    }
+}
