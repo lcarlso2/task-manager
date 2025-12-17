e94384416e70f5cd9f1c869dfe2ad2050f8df70a
Lucas Carlson <lucas.carlson@neighborlysoftware.com>
2025-12-16 19:10:11 -0500

    refactor: clean up app.css and group styles by responsibility

--- frontend/todo-ui/src/App.tsx
+++ frontend/todo-ui/src/App.tsx
@@ -6,7 +6,9 @@ export default function App() {
   return (
     <main>
       <h1>Todos</h1>
-      <CreateTodo />
+      <div className="create-todo-wrapper">
+        <CreateTodo />
+      </div>
       <TodoList />
     </main>
   );
