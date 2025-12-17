7f71685b46f7eb5b0ac59dfed495708c7b82a29d
Lucas Carlson <lucas.carlson@neighborlysoftware.com>
2025-12-16 19:08:00 -0500

    refactor: replace inline spacing with CSS in PageSizeSelector

--- frontend/todo-ui/src/components/PageSizeSelector.tsx
+++ frontend/todo-ui/src/components/PageSizeSelector.tsx
@@ -5,9 +5,12 @@ type PageSizeSelectorProps = {
 
 export function PageSizeSelector({ value, onChange }: PageSizeSelectorProps) {
   return (
-    <label>
-      Todos per page:&nbsp;
-      <select value={value} onChange={(e) => onChange(Number(e.target.value))}>
+    <label className="page-size-selector">
+      <span>Items per page:</span>
+      <select
+        value={value}
+        onChange={(e) => onChange(Number(e.target.value))}
+      >
         <option value={10}>10</option>
         <option value={20}>20</option>
         <option value={50}>50</option>
